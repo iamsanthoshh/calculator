4cdb6db7447a6ddf048e6b2d6f08c303b8740529
iamsanthoshh <santhoshjikumar1992@gmail.com>
2023-06-19 22:13:37 +0530

    final

--- calculator/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Button = ({handleClick,label}) => {
-
-    return (
-    <input type="button"    value={label}  onClick = {handleClick}
-    />
-    );
-};
-export default Button;
